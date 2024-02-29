80759681b50075a5065881d1d6ae02a1843a82b6
parungaokervin1@gmail.com <parungaokervin1@gmail.com>
2024-02-28 20:33:49 -0500

    1-5

---
+++ JUnitTest/TesterAjouter.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class TesterAjouter {
+    Configuration configuration;
+    @Test
+    void ajouterFonctionne() {
+        Composant comp1 = new Composant("CPU", "AMD", "Ryzen 5 5600", 190);
+        Composant nouvComp = new Composant("GPU", "AMD", "RX 6700 XT", 500);
+        configuration = new Configuration("Build AMD", 1250, new Composant[]{nouvComp});
+
+        assertTrue(configuration.ajouter(comp1));
+    }
+
+    void ajouterFonctionnePas() {
+        Composant comp1 = new Composant("CPU", "AMD", "Ryzen 5 5600", 190);
+        Composant nouvComp = new Composant("CPU", "Intel", "Core i5-11600k");
+        configuration = new Configuration("Build AMD", 1250, new Composant[]{nouvComp});
+
+        assertTrue(configuration.ajouter(comp1));
+    }
+}
