9ad1e7336951c7b07ff806d3da30a2d00a39a88d
parungaokervin1@gmail.com <parungaokervin1@gmail.com>
2024-02-28 20:45:23 -0500

    1-6

--- JUnitTest/TesterAjouter.java
+++ JUnitTest/TesterAjouter.java
@@ -9,16 +9,26 @@ class TesterAjouter {
     void ajouterFonctionne() {
         Composant comp1 = new Composant("CPU", "AMD", "Ryzen 5 5600", 190);
         Composant nouvComp = new Composant("GPU", "AMD", "RX 6700 XT", 500);
-        configuration = new Configuration("Build AMD", 1250, new Composant[]{nouvComp});
+        configuration = new Configuration("Build AMD", 1250, new Composant[]{comp1});
 
-        assertTrue(configuration.ajouter(comp1));
+
+        assertTrue(configuration.ajouter(nouvComp));
     }
     @Test
     void ajouterFonctionnePas() {
         Composant comp1 = new Composant("CPU", "AMD", "Ryzen 5 5600", 190);
         Composant nouvComp = new Composant("CPU", "Intel", "Core i5-11600k", 220);
-        configuration = new Configuration("Build AMD", 1250, new Composant[]{nouvComp});
+        configuration = new Configuration("Build AMD", 1250, new Composant[]{comp1});
 
-        assertTrue(configuration.ajouter(comp1));
+        assertFalse(configuration.ajouter(nouvComp));
+    }
+
+    @Test
+    void ajouterFonctionnePasPrixHaut() {
+        Composant comp1 = new Composant("CPU", "AMD", "Ryzen 5 5600", 190);
+        Composant nouvComp = new Composant("GPU", "Nvidia", "RTX 4090", 2000);
+        configuration = new Configuration("Build AMD", 1250, new Composant[]{comp1});
+
+        assertFalse(configuration.ajouter(nouvComp));
     }
 }
